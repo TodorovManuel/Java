36baddbfe11bb8e980bc9f08efa361a59777b9d1
TodorovManuel <alumno24.todorov.manuel@ipm.edu.ar>
2023-03-21 11:55:38 -0300

    ej5

---
+++ martinSolo/src/Fecha.java
@@ -0,0 +1,47 @@
+public class Fecha {
+    private int dia;
+    private int mes;
+    private int anio;
+
+    public Fecha() {
+        this.dia = 6;
+        this.mes = 8;
+        this.anio = 2005;
+    }
+
+    public Fecha(int dia, int mes, int anio) {
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+        valida(dia, mes, anio);
+    }
+    public int valida(int dia, int mes, int anio){
+        if(dia < 1 || dia > 31) {
+            dia=1;
+        }
+
+        if(mes < 1 || mes > 12) {
+           mes=1;
+        }
+
+        if(anio < 1) {
+           anio=1900;
+        }
+        System.out.println(dia + " " + mes + " " + anio);
+        return dia;
+    }
+
+    public int diasMes(int mes) {
+        int dias;
+        if(mes==2) {
+            dias = 28;
+        } else if(mes%2 == 0) {
+            dias=30;
+        } else {
+            dias=31;
+        }
+        return dias;
+    }
+}
+
+
--- martinSolo/src/Main.java
+++ martinSolo/src/Main.java
@@ -20,7 +20,15 @@ public class Main {
       //  cochecito.frenar(freno);
       //  cochecito.velocidadActual(cochecito.getVelocidad());
 
-        Persona personita = new Persona("Benicio", 5, "Blanco encalada 2234");
-        personita.muestraDatos();
+      //  Persona personita = new Persona("Benicio", 5, "Blanco encalada 2234");
+      //  personita.muestraDatos();
+
+          Fecha fechita = new Fecha(6, 8, 2005);
+          fechita.valida(40,0,0);
+          Scanner lectura = new Scanner(System.in);
+        System.out.println("Ingrese el mes del cual quiere saber los dias, ingrese en formato del 1 al 12");
+        int mesIngresado = lectura.nextInt();
+        fechita.diasMes(mesIngresado);
+        System.out.println("El mes " + mesIngresado + " tiene " + fechita.diasMes(mesIngresado));
     }
 }
