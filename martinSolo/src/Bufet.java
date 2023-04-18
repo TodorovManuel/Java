25beb40d61d40673ceb639216a1ca87e9b3a49d2
TodorovManuel <alumno24.todorov.manuel@ipm.edu.ar>
2023-04-18 10:15:51 -0300

    buffetTerminado

--- martinSolo/src/Bufet.java
+++ martinSolo/src/Bufet.java
@@ -59,35 +59,115 @@ public class Bufet {
     }
 
     public void modificarAlumno() {
-        
+        Scanner lecturaQueAlumno = new Scanner((System.in));
+        System.out.println("Que alumno quiere modificar?: ");
+        String alumnoModificar = lecturaQueAlumno.next();
+        for(Persona listaPersona: listaPersonas) {
+            if(listaPersona instanceof Alumno && listaPersona.getNombre()==alumnoModificar) {
+                System.out.println("Ingrese nuevo nombre: ");
+                String nuevoNombreIngresado = lecturaQueAlumno.next();
+                listaPersona.setNombre(nuevoNombreIngresado);
+                System.out.println("Ingrese nuevo apellido: ");
+                String nuevoApellidoIngresado = lecturaQueAlumno.next();
+                listaPersona.setApellido(nuevoApellidoIngresado);
+                System.out.println("El alumno fue modificado correctamente");
+            }
+        }
     }
 
     public void modificarProfesor() {
-
+        Scanner lecturaQueProfesor = new Scanner((System.in));
+        System.out.println("Que profesor quiere modificar?: ");
+        String profesorModificar = lecturaQueProfesor.next();
+        for(Persona listaPersona: listaPersonas) {
+            if(listaPersona instanceof Profesor && listaPersona.getNombre()==profesorModificar) {
+                System.out.println("Ingrese nuevo nombre: ");
+                String nuevoNombreIngresado = lecturaQueProfesor.next();
+                listaPersona.setNombre(nuevoNombreIngresado);
+                System.out.println("Ingrese nuevo apellido: ");
+                String nuevoApellidoIngresado = lecturaQueProfesor.next();
+                listaPersona.setApellido(nuevoApellidoIngresado);
+                System.out.println("El profesor fue modificado correctamente");
+            }
+        }
     }
 
     public void modificarPlato() {
-
+        Scanner lecturaQuePlato = new Scanner((System.in));
+        System.out.println("Que plato quiere modificar?: ");
+        String platoModificar = lecturaQuePlato.next();
+        for(Plato listaPlato: listaPlatos) {
+            if(listaPlato.getNombrePlato()==platoModificar) {
+                System.out.println("Ingrese nuevo nombre del plato: ");
+                String nuevoNombreIngresado = lecturaQuePlato.next();
+                listaPlato.setNombrePlato(nuevoNombreIngresado);
+                System.out.println("Ingrese nuevo precio: ");
+                double nuevoPrecioIngresado = lecturaQuePlato.nextDouble();
+                listaPlato.setPrecioPlato(nuevoPrecioIngresado);
+                System.out.println("El plato fue modificado correctamente");
+            }
+        }
     }
 
     public void modificarPedido() {
-
+        Scanner lecturaQuePedido = new Scanner((System.in));
+        System.out.println("Que pedido quiere modificar?: ");
+        String pedidoModificar = lecturaQuePedido.next();
+        for(Pedido listaPedido: listaPedidos) {
+            if(listaPedido.getFechaCreacion()==pedidoModificar) {
+                System.out.println("Ingrese el nuevo horario de entrega: ");
+                String nuevoHorarioIngresado = lecturaQuePedido.next();
+                listaPedido.setFechaCreacion(nuevoHorarioIngresado);
+                System.out.println("Ingrese si esta entregado 'true' y si no 'false': ");
+                boolean nuevoEntregadoIngresado = lecturaQuePedido.hasNext();
+                listaPedido.setFechaCreacion(String.valueOf(nuevoEntregadoIngresado));
+                System.out.println("El pedido fue modificado correctamente");
+            }
+        }
     }
 
     public void eliminarAlumno() {
-
+        Scanner lecturaAlumnoEliminar = new Scanner((System.in));
+        System.out.println("Que alumno quiere eliminar?: ");
+        String alumnoEliminar = lecturaAlumnoEliminar.next();
+        for(Persona listaPersona: listaPersonas) {
+            if (listaPersona instanceof Alumno && listaPersona.getNombre()==alumnoEliminar) {
+                listaPersonas.remove(listaPersona);
+            }
+        }
     }
 
     public void eliminarProfesor() {
-
+        Scanner lecturaProfesorEliminar = new Scanner((System.in));
+        System.out.println("Que profesor quiere eliminar?: ");
+        String profesorEliminar = lecturaProfesorEliminar.next();
+        for(Persona listaPersona: listaPersonas) {
+            if (listaPersona instanceof Profesor && listaPersona.getNombre()==profesorEliminar) {
+                listaPersonas.remove(listaPersona);
+            }
+        }
     }
 
     public void eliminarPedido() {
-
+        Scanner lecturaPedidoEliminar = new Scanner((System.in));
+        System.out.println("Que pedido quiere eliminar?: ");
+        String pedidoEliminar = lecturaPedidoEliminar.next();
+        for(Pedido listaPedido: listaPedidos) {
+            if (listaPedido.getNombre()==pedidoEliminar) {
+                listaPedidos.remove(listaPedido);
+            }
+        }
     }
 
     public void eliminarPlato() {
-
+        Scanner lecturaPlatoEliminar = new Scanner((System.in));
+        System.out.println("Que plato quiere eliminar?: ");
+        String platoEliminar = lecturaPlatoEliminar.next();
+        for(Plato listaPlato: listaPlatos) {
+            if (listaPlato.getNombrePlato()==platoEliminar) {
+                listaPersonas.remove(listaPlato);
+            }
+        }
     }
 
     public void menu() {
@@ -184,6 +264,24 @@ public class Bufet {
                 eliminarPlato();
             default:
                 System.out.println("Que queres eliminar chabon/chabona??????!!!");
+        }
+    }
+
+    public void listaMostrar() {
+        for(Persona listaPersona : listaPersonas) {
+
+            for (Pedido listaPedido : listaPedidos) {
+                System.out.println( "Fecha de creacion: " + listaPedido.getFechaCreacion() + " Horario de entrega: " + listaPedido.getHorarioEntrega());
+                for (Plato listaPlato : listaPlatos) {
+                    if(listaPersona instanceof Profesor) {
+                        double descuentoAplicar = ((Profesor) listaPersona).getDescuento();
+                        System.out.println("Plato: " + listaPlato.getNombrePlato() + " Precio: " + (listaPlato.getPrecioPlato() - (descuentoAplicar*listaPlato.getPrecioPlato()/100)));
+                    } else {
+                        System.out.println("Plato: " + listaPlato.getNombrePlato() + " Precio: " + listaPlato.getPrecioPlato());
+                    }
+
+                }
+            }
         }
     }
 }
